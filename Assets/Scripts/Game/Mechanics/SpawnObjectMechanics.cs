f02243442a6ebc078e46cfed943cec278a364956
Rodion <rzhiglyaev@mail.ru>
2021-10-25 23:06:37 +0300

    fix OnValidate

--- Assets/Scripts/Game/Mechanics/SpawnObjectMechanics.cs
+++ Assets/Scripts/Game/Mechanics/SpawnObjectMechanics.cs
@@ -8,10 +8,10 @@ namespace Game.Mechanics
     [RequireComponent(typeof(CircleCollider2D))]
     public class SpawnObjectMechanics : MonoBehaviour
     {
-        [SerializeField] private int _maxSize = 2;
-        [SerializeField] private float _maxLifeTime = 5.0f;
+        [Min(1)] [SerializeField] private int _maxSize = 2;
+        [Min(1)] [SerializeField] private float _maxLifeTime = 5.0f;
         [SerializeField] private float fine = -1f;
-        [SerializeField] private float maxReward = 1f;
+        [Min(1)] [SerializeField] private float maxReward = 1f;
         
         private float _spawnTime;
         private float _currentDeltaTime;
@@ -20,16 +20,8 @@ namespace Game.Mechanics
 
         private void OnValidate()
         {
-            if (_maxSize < 1)
-                _maxSize = 1;
-            if (maxReward < 1)
-                maxReward = 1;
             if (fine > -1f)
                 fine = -1f;
-            if (_maxSize < 1)
-                _maxSize = 1;
-            if (_maxLifeTime < 1)
-                _maxLifeTime = 1;
         }
 
         void Start()
